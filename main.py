19fd4c51bae103e63303c5788a0f1b36b8dfbb4c
ryanweeks <ryan.weeks@mymail.champlain.edu>
2018-01-05 19:40:39 -0500

    Very simple audio player

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import vlc
+import pygame
 
-print('working')
+pygame.mixer.init()
+pygame.mixer.music.load("RENTAL-BROCKHAMPTON.mp3")
+pygame.mixer.music.play()
+
+input("Press Enter to continue...")
