69790b0730655ad6e49a465ecaa8e34e0cf8c3c1
ryanweeks <ryan.weeks@mymail.champlain.edu>
2018-01-05 19:47:23 -0500

    Now asks for path to play

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 
+song = input("Enter the path of the music file...")
+
 pygame.mixer.init()
-pygame.mixer.music.load("RENTAL-BROCKHAMPTON.mp3")
+pygame.mixer.music.load(song)
+
+print("Now playing, " + song)
+
 pygame.mixer.music.play()
 
 input("Press Enter to continue...")
